f031fe9260a93fd709f3809f671666c4385e866f
Nkaka23dev <nkakaeric96@gmail.com>
2022-12-17 19:01:18 +0200

    Feat: Finished add all cards

--- components/Card.jsx
+++ components/Card.jsx
@@ -3,28 +3,26 @@ import Image from 'next/image'
 import Button from './UI/Button'
 import CardBtn from './UI/CardBtn'
 
-export default function Card({ posts }) {
+export default function Card({ post }) {
     return (
         <>
-            {posts.map((post) => (
-                <div key={post.id} className='bg-[#e9eef1] flex flex-col relative font-DINWebPro'>
-                    {/* Title */}
-                    <div>
-                        <Image src={post.imgUrl} alt="logo" className='w-full h-full object-cover object-center aspect-video' width={500} height={500}
-                        />
-                    </div>
-                    <div className='px-5 pt-5'>
-                        <p className='text-xl text-primary'>{post.title}</p>
-                        {/* Body */}
-                        <p className='mt-3'>{post.description}</p>
-                    </div>
-                    {/* Footer */}
-                    <div className='px-5 py-5'>
-                        <Button title = "More" />
-                   </div>
-                    <CardBtn styles="absolute px-3 top-3 left-3 " title="Cloud" />
+            <div className='bg-[#e9eef1] flex flex-col relative font-DINWebPro'>
+                {/* Title */}
+                <div>
+                    <Image src={post.imgUrl} alt="image" className='w-full h-full object-cover object-center aspect-video' width={500} height={500}
+                    />
                 </div>
-            ))}
+                <div className='px-5 pt-5'>
+                    <p className='text-xl tracking-wider text-primary'>{post.title}</p>
+                    {/* Body */}
+                    <p className='mt-3 text-xl'>{post.description}</p>
+                </div>
+                {/* Footer */}
+                <div className='px-5 py-5'>
+                    <Button title="More" />
+                </div>
+                <CardBtn styles="absolute px-3 top-3 left-3 " title={post.buttonTitle} />
+            </div>
         </>
     )
 }
--- components/sections/Services.jsx
+++ components/sections/Services.jsx
@@ -1,15 +1,41 @@
 import React from 'react'
 import Card from '../Card'
-const data = [{
-    id:1,
-    imgUrl: '/img/mata.png',
-    title: 'We accompany your Journey2Cloud',
-    description: 'We analyze your application landscapes and transfer them step by step into a sustainable model.'
-}]
+const data = [
+    {
+        id: 1,
+        imgUrl: '/img/mata.png',
+        title: 'We accompany your Journey2Cloud',
+        buttonTitle: 'Cloud',
+        description: 'We analyze your application landscapes and transfer them step by step into a sustainable model.'
+    },
+    {
+        id: 1,
+        imgUrl: '/img/punch.png',
+        title: 'Cyber Security',
+        buttonTitle: 'Cyber Security Check',
+        description: 'Our cyber services help you before, during and after any damage might occur by providing secure applications development, protection for your IT and information security.'
+    },
+    {
+        id: 1,
+        imgUrl: '/img/typing.png',
+        title: 'Enterprise Service Management',
+        buttonTitle: 'Processes',
+        description: 'Benefit from the market leader for service management. More than 1,000 customers across all sectors'
+    },
+    {
+        id: 1,
+        imgUrl: '/img/iot.png',
+        title: 'Enterprise Service Management',
+        buttonTitle: 'IoT',
+        description: 'Benefit from the market leader for service management. More than 1,000 customers across all sectors'
+    },
+]
 export default function Services() {
-  return (
-    <div className='p-5'>
-    <Card posts={data} />
-    </div>
-  )
+    return (
+        <div className='p-5 space-y-5'>
+            {data.map((value) => (
+                <Card key={value.id} post={value} />
+            ))}
+        </div>
+    )
 }
